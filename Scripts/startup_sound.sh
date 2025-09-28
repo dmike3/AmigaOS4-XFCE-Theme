9a98470125c867e1c6167b1eef21d7d5aa1555fc
niter3 <mfenton1@gmail.com>
2025-09-28 09:44:23 -0400

    Updated readme file

--- Scripts/startup_sound.sh
+++ Scripts/startup_sound.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ffplay -nodisp -autoexit $HOME/Documents/Amiga_Sounds/BootSounds/Hirasawa.wav &
+ffplay -nodisp -autoexit $HOME/Documents/Amiga_Sounds/AmigaOS-BootSounds-By_Nykk/AmigaOS-BootSounds-by_Nykk/AmigaOS-BootSound-v2-by_Nykk-NoVoices.wav &
